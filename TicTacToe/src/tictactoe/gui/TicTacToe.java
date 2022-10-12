57151ac2a2703ef846dc50330d96cd34648d65c1
andreas14101 <andreasshansen@live.dk>
2022-10-12 10:10:49 +0200

    "update v0.8"

--- TicTacToe/src/tictactoe/gui/TicTacToe.java
+++ TicTacToe/src/tictactoe/gui/TicTacToe.java
@@ -10,6 +10,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -28,8 +29,8 @@ public class TicTacToe extends Application
         
         Scene scene = new Scene(root);
         
-        
         stage.setScene(scene);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("images/logo.png")));
         stage.setResizable(false);
         stage.setTitle("Tic Tac Toe");
         stage.centerOnScreen();
